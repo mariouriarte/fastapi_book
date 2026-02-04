92f3458f1774496c1d70e377c3d77a4056ffd970
Mario Uriarte <mario.uriarte@gmail.com>
2026-02-04 10:02:06 -0400

    fix install sh in ch3

--- c3/install.sh
+++ c3/install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-python -m venv venv
+if [ ! -d "venv" ]; then
+    # Aquí pones lo que quieres hacer si NO existe
+    echo '> No exixte venv, se creara...'
+    python -m venv venv
+fi
+
 source venv/bin/activate
 
 pip install fastapi uvicorn httpie requests httpx pytest
