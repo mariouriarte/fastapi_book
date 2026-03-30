8a25de3b1dad9bcf7f7dc704f513085f9a4b60d7
Mario Uriarte <mario.uriarte@gmail.com>
2026-03-29 23:13:26 -0400

    Chapter 14

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from web import explorer, creature, user
+from web import explorer, creature, user, ai
 
 app = FastAPI()
 
 app.include_router(explorer.router)
 app.include_router(creature.router)
 # app.include_router(user.router)
+app.include_router(ai.router)
 
 if __name__ == "__main__":
     import uvicorn
