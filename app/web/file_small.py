f24559a53d3e6533c03e06589d3d3b1fdab7a413
Mario Uriarte <mario.uriarte@gmail.com>
2026-04-06 16:18:47 -0400

    upload file

--- app/web/file_small.py
+++ app/web/file_small.py
@@ -1,5 +1,7 @@
 from fastapi import File
-from fastapi import APIRouter 
+from fastapi import APIRouter
+from fastapi import UploadFile
+from fastapi.responses import FileResponse
 
 router = APIRouter(prefix= "/file")
 
@@ -9,3 +11,14 @@ async def upload_small_file(small_file: bytes = File()) -> str:
 
 # http -f -b POST http://localhost:8000/file/small small_file@1KB.bin
 # -f de form ya que se sube un archivo, no se usa json
+
+# para crear un archivo
+# dd if=/dev/urandom of=1KB.bin bs=1024 count=1
+
+@router.get("/small/{name}")
+async def download_small_file(name):
+    return FileResponse(name)
+
+@router.post("/big")
+async def upload_big_file(big_file: UploadFile) -> str:
+    return f"file size: {big_file.size}, name: {big_file.filename}"
